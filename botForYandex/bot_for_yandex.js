dba2cb0e0e5984d297d4b96edc16aec8b9190e6d
ku4keee <ku4keee@hotmail.com>
2021-03-22 11:48:28 +0300

    Update bot_for_yandex.js
    Изменения :

--- botForYandex/bot_for_yandex.js
+++ botForYandex/bot_for_yandex.js
@@ -1,13 +1,27 @@
 // ==UserScript==
-// @name         bot for Yandex
+// @name         bot for yandex v2.0
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://yandex.ru/*
+// @match        https://crushdrummers.ru/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
-let keyWords = ["Как звучит флейта","Валторна","Тромбон","Кларнет","Фагот","Гобой","Саксофон"];
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+let sites = {
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["Как звучит флейта","Валторна","Тромбон","Кларнет","Фагот","Гобой","Саксофон"],
+    "crushdrummers.ru":["Барабанное шоу","Заказать шоу барабанщиков","Барабанное шоу в Москве"]
+}
+let site = Object.keys(sites)[Math.floor(Math.random()*Object.keys(sites).length)];
+let keyWords = sites[site];
 let randomIndex = Math.floor(Math.random()*keyWords.length);
 let yandexInput = document.getElementById('text');
 let butt = document.getElementsByClassName("button_theme_websearch")[0];
@@ -15,6 +29,7 @@ let links = document.links;
 let keyword = keyWords[randomIndex];
 if(butt!=undefined){
    let i = 0;
+   document.cookie = "site="+site;
    let timerId = setInterval(()=>{
        yandexInput.value += keyword[i++];
        if(i==keyword.length){
@@ -22,16 +37,27 @@ if(butt!=undefined){
          butt.click();
        }
    },250);
-}else {
+}else if(location.hostname == "yandex.ru") {
+    site = getCookie("site");
     let nextYandexPage = true;
+    let currentYandexPage = document.getElementsByClassName('pager__item_current_yes')[0].innerText;
     for(let i=0; i<links.length; i++){
         let link = links[i]
-        if(link.href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
+        if(link.href.indexOf(site)!=-1){
           nextYandexPage=false
           link.removeAttribute('target')
           link.click();
           break;
         }
     }
-    if(nextYandexPage) document.getElementsByClassName('pager__item_kind_next ').click();
+    if(nextYandexPage && currentYandexPage<11) setTimeout(()=>{document.getElementsByClassName('pager__item_kind_next ').click()},1500);
+    else if (currentYandexPage == 11) location.href = "https://yandex.ru/";
+}else{
+    setInterval(()=>{
+      if(Math.random()>=0.8) location.href = "https://yandex.ru/";
+      let link = links[Math.floor(Math.random()*links.length)];
+      if (link.href.indexOf(location.hostname)!=-1){
+          link.click();}
+      },3000);
 }
+
